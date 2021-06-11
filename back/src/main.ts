de138c58e8458a782cf81f66d3734799a3cc4a39
Alexandre BRUN GIGLIO <alexandre.brungiglio@nextalys.com>
2021-06-11 11:27:39 +0200

    improve main.ts

--- back/src/main.ts
+++ back/src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
 
   const config = new DocumentBuilder()
     .setTitle('API template')
@@ -11,9 +12,21 @@ async function bootstrap() {
     .setVersion('1.0')
     .addTag('doc')
     .build();
-  const document = SwaggerModule.createDocument(app, config);
 
-  SwaggerModule.setup('api', app, document);
+  const document = SwaggerModule.createDocument(app, config);
+  app.use('/api/docs/swagger.json', (req: any, res: any) => {
+    res.send(document);
+  });
+
+  SwaggerModule.setup('swagger', app, null, {
+    swaggerUrl: `/api/docs/swagger.json`,
+    explorer: true,
+    swaggerOptions: {
+      docExpansion: 'list',
+      filter: true,
+      showRequestDuration: true,
+    },
+  });
   await app.listen(3000);
 }
 bootstrap();
