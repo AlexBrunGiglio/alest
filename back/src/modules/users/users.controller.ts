c570b64e8cde432250673c103802c1e9bd3a9f3e
Alexandre BRUN GIGLIO <alexandre.brungiglio@nextalys.com>
2021-06-15 10:37:12 +0200

    fix

--- back/src/modules/users/users.controller.ts
+++ back/src/modules/users/users.controller.ts
@@ -1,11 +1,12 @@
 import { Controller, Get, HttpCode, UseGuards } from '@nestjs/common';
-import { ApiBearerAuth, ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { BaseController } from '../../base/base.controller';
 import { Roles } from '../../base/services/roles.decorator';
 import { GetUsersResponse } from './user-dto';
 import { UsersService } from './users.service';
 import { RolesList } from '../../../../shared/shared-constant'
 @Controller('users')
+@ApiTags('users')
 export class UsersController extends BaseController {
     constructor(
         private readonly usersService: UsersService,
