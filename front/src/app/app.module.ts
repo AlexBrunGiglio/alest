f32bb2940dec868ed0964c1f785d16bc36b4c8d6
Alexandre BRUN GIGLIO <alexandre.brungiglio@nextalys.com>
2021-06-17 19:45:04 +0200

    fix style

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -9,6 +9,16 @@ import { UsersListComponent } from './pages/admin/users-list/users-list.componen
 import { EditUsersComponent } from './pages/admin/edit-users/edit-users.component';
 import { LoginComponent } from './pages/login/login.component';
 import { AdminDrawerModule } from './components/admin-drawer/admin-drawer.module';
+import { SharedModule } from './shared.module';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { RouterModule } from '@angular/router';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @NgModule({
   declarations: [
@@ -24,6 +34,15 @@ import { AdminDrawerModule } from './components/admin-drawer/admin-drawer.module
     BrowserAnimationsModule,
     AppRoutingModule,
     AdminDrawerModule,
+    CommonModule,
+    FormsModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+    MatSelectModule,
+    RouterModule,
+    MatProgressSpinnerModule,
+    MatFormFieldModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- front/src/app/shared.module.ts
+++ front/src/app/shared.module.ts
@@ -7,6 +7,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSelectModule } from '@angular/material/select';
 import { RouterModule } from "@angular/router";
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatFormFieldModule } from '@angular/material/form-field';
 @NgModule({
     imports: [
         CommonModule,
@@ -16,7 +17,8 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
         MatButtonModule,
         MatSelectModule,
         RouterModule,
-        MatProgressSpinnerModule
+        MatProgressSpinnerModule,
+        MatFormFieldModule,
     ],
     declarations: [
     ],
