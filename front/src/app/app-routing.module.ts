877f554b5de5103a137a53a54edd567ed0d1e19a
Alexandre BRUN GIGLIO <alexandre.brungiglio@nextalys.com>
2021-06-17 09:43:41 +0200

    menu improve

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './pages/admin/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
 import { RoutesList } from './routes/routes';
 
 const routes: Routes = [
@@ -12,6 +13,10 @@ const routes: Routes = [
   {
     path: RoutesList.AdminHome,
     component: HomeComponent,
+  },
+  {
+    path: RoutesList.Login,
+    component: LoginComponent
   }
 ]
 
