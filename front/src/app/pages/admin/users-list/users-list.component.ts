79ef31c3ae866799387f29ad6b23692d7436cec8
Alexandre BRUN GIGLIO <alexandre.brungiglio@nextalys.com>
2021-06-19 01:49:32 +0200

    start user list

--- front/src/app/pages/admin/users-list/users-list.component.ts
+++ front/src/app/pages/admin/users-list/users-list.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-users-list',
   templateUrl: './users-list.component.html',
-  styleUrls: ['./users-list.component.scss']
+  styleUrls: ['./users-list.component.scss', '../../../base/base-list.scss']
 })
 export class UsersListComponent implements OnInit {
 
