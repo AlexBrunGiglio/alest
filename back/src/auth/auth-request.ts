15910ea882f5984671734d77b86e79e2688f80a0
Alexandre BRUN GIGLIO <alexandre.brungiglio@nextalys.com>
2021-06-21 17:40:32 +0200

    auth request

---
+++ back/src/auth/auth-request.ts
@@ -0,0 +1,17 @@
+import { ApiProperty } from "@nestjs/swagger";
+
+export class RegisterRequest {
+    @ApiProperty({ description: "user email", type: String })
+    mail: string;
+    @ApiProperty({ description: "user password", type: String })
+    password: string;
+    @ApiProperty({ description: "user username", type: String, })
+    username: string;
+}
+
+export class LoginViewModel {
+    @ApiProperty()
+    username: string;
+    @ApiProperty()
+    password: string;
+}
