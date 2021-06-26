e40cc620ef1da4f64b428c107592e1b0c9280fe3
Alexandre BRUN GIGLIO <alexandre.brungiglio@nextalys.com>
2021-06-26 02:13:50 +0200

    start get users request

--- front/src/app/pages/admin/users-list/users-list.component.ts
+++ front/src/app/pages/admin/users-list/users-list.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { UserDto, UsersService } from '../../../../providers/api-client.generated';
+import { BaseComponent } from '../../../base/base.component';
 
 @Component({
   selector: 'app-users-list',
   templateUrl: './users-list.component.html',
   styleUrls: ['./users-list.component.scss', '../../../base/base-list.scss']
 })
-export class UsersListComponent implements OnInit {
-
-  constructor() { }
+export class UsersListComponent extends BaseComponent implements OnInit {
+  users: UserDto[];
+  constructor(
+    private userService: UsersService,
+  ) {
+    super();
+    this.init();
+  }
 
   ngOnInit(): void {
+  }
+
+  async init() {
+    const getUsersResponse = await this.userService.getAllUsers().toPromise();
+    if (!getUsersResponse.success)
+      return console.warn(getUsersResponse.message);
+    this.users = getUsersResponse.users;
   }
 
 }
