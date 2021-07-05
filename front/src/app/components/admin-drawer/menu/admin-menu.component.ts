301c7f17398bbaa648989ac02bc805a0c74a0cc7
Alexandre BRUN GIGLIO <alexandre.brungiglio@nextalys.com>
2021-07-05 16:18:46 +0200

    call dialog custom

--- front/src/app/components/admin-drawer/menu/admin-menu.component.ts
+++ front/src/app/components/admin-drawer/menu/admin-menu.component.ts
@@ -1,4 +1,7 @@
 import { Component, ViewEncapsulation } from "@angular/core";
+import { Router } from "@angular/router";
+import { AuthDataService } from "../../../../services/auth-data.service";
+import { DialogService } from "../../../../services/dialog.service";
 import { BaseComponent } from "../../../base/base.component";
 
 @Component({
@@ -9,7 +12,19 @@ import { BaseComponent } from "../../../base/base.component";
 })
 
 export class AdminSidebarComponent extends BaseComponent {
-    constructor() {
+    constructor(
+        private router: Router,
+        public dialogService: DialogService,
+    ) {
         super();
+    }
+
+    redirectToProfile() {
+        this.router.navigateByUrl('/' + this.RoutesList.AdminUsers + '/' + AuthDataService.currentUser.id);
+    }
+
+    async logout() {
+        const dialog = await this.dialogService.showDialog({ header: "Deconnexion", text: "Souhaitez vous vraiment vous déconnecter ?", okLabel: "Confirmer", cancelLabel: "Annuler" });
+        console.log("🚀 ~ AdminSidebarComponent ~ logout ~ dialog", dialog);
     }
 }
